315debca104b2d1745f2407add12d22768e96e5e
LojainHassan <lojainhteabat@gmail.com>
2026-07-04 19:05:25 +0300

    Add project files.

---
+++ src/MainAppBackend.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainAppBackend.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
---
+++ src/MainAppBackend.Domain/Common/SoftDeleteEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainAppBackend.Domain.Common;
+
+public abstract class SoftDeleteEntity : AuditableEntity
+{
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
+    public string? DeletedBy { get; set; }
+}
